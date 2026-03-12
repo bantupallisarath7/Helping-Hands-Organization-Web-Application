f0147e3eb78576b2b8f0eeed4081fdda1db7d397
Sarath Kumar Bantupalli <bantupallisarath@gmail.com>
2026-03-12 20:50:41 +0530

    update signout commit

--- backend/Controllers/userControllers/signOut.js
+++ backend/Controllers/userControllers/signOut.js
@@ -5,7 +5,7 @@ const signOut = async (req, res, next) => {
         res.clearCookie("access_token");
         res.status(201).json({
             success: true,
-            message: `${req.user.role === "admin" ? "Admin" : "User"} Sign out successfully`
+            message: `Sign out successfully`
         })
     }
     catch (error) {
